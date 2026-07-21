6ed0b51f5aad39c6ad541c3d77fe5ec05071345f
IsaquiasMarques <patisaquias2000@gmail.com>
2026-07-21 23:33:24 +0100

    changing vercel environment variables names

--- generate-env.js
+++ generate-env.js
@@ -14,9 +14,9 @@ if (!fs.existsSync(envDir)) {
 const envContent = `
 export const environment = {
   production: true,
-  server: '${process.env['SERVER_URL'] || process.env['server'] || 'http://localhost:8000'}',
-  supabaseUrl: '${process.env['SUPABASE_URL'] || 'https://xyz.supabase.co'}',
-  supabaseAnonKey: '${process.env['SUPABASE_ANON_KEY'] || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'}'
+  server: '${process.env['server'] || 'http://localhost:8000'}',
+  supabaseUrl: '${process.env['supabaseUrl'] || 'https://xyz.supabase.co'}',
+  supabaseAnonKey: '${process.env['supabaseAnonKey'] || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'}'
 };
 `;
 
