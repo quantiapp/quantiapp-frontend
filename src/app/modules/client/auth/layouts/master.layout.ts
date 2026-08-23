a86bf7695358a04b97ba78d6a7a682681eefc952
IsaquiasMarques <patisaquias2000@gmail.com>
2026-08-23 22:41:27 +0100

    style: implement desktop app frame container layout in auth master layout

--- src/app/modules/client/auth/layouts/master.layout.ts
+++ src/app/modules/client/auth/layouts/master.layout.ts
@@ -7,22 +7,29 @@ import { Darkable } from "@shared/directives/darkable";
   selector: 'app-master',
   imports: [RouterOutlet, MotionedHeight, RouterLink, Darkable],
   template: `
-    <div class="master bg-white h-[100dvh]" appDarkable="dark:bg-(color:--dm-bg)">
-      <div class="panel-container h-full flex flex-col gap-5 justify-center">
-        <div
-        data-aos="zoom-in-down"
-        class="container-header flex flex-col gap-[0.625rem] justify-center items-center">
-          <div class="logo w-[3.438rem] h-[3.438rem] rounded-full border border-(color:--primary)">
-            <a routerLink="/">
-              <img src="/static/primary-brand.svg" class="w-full h-full" alt="Quantiapp logo">
-            </a>
+    <!-- Fundo de ecrã inteiro no PC com tom escuro premium -->
+    <div class="master-wrapper min-h-screen bg-[#121212] flex items-center justify-center p-0 md:py-6 md:px-4">
+      
+      <!-- Container com limites de smartphone no Desktop -->
+      <div class="app-frame w-full max-w-[480px] min-h-[100dvh] md:min-h-[850px] md:max-h-[90dvh] md:rounded-[2.25rem] md:shadow-[0_20px_50px_rgba(0,0,0,0.6)] overflow-y-auto overflow-x-hidden bg-white relative flex flex-col" appDarkable="dark:bg-(color:--dm-bg)">
+        
+        <div class="panel-container h-full flex flex-col gap-5 justify-center py-8">
+          <div
+          data-aos="zoom-in-down"
+          class="container-header flex flex-col gap-[0.625rem] justify-center items-center">
+            <div class="logo w-[3.438rem] h-[3.438rem] rounded-full border border-(color:--primary)">
+              <a routerLink="/">
+                <img src="/static/primary-brand.svg" class="w-full h-full" alt="Quantiapp logo">
+              </a>
+            </div>
+            <span class="app-name font-bold text-xl text-(color:--secondary)" appDarkable="dark:text-(color:--dm-secondary)">QuantiAPP</span>
           </div>
-          <span class="app-name font-bold text-xl text-(color:--secondary)" appDarkable="dark:text-(color:--dm-secondary)">QuantiAPP</span>
+          <div
+          class="container-content flex flex-col" appMotionedHeight [RouterOutletable]="true">
+            <router-outlet></router-outlet>
+          </div>
         </div>
-        <div
-        class="container-content flex flex-col" appMotionedHeight [RouterOutletable]="true">
-          <router-outlet></router-outlet>
-        </div>
+
       </div>
     </div>
   `,
